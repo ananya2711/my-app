286b6c52a2580096783aa6a617d29b1f65f4c654
ananya2711 <54437908+ananya2711@users.noreply.github.com>
2022-11-29 21:59:17 +0530

    working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,43 @@
 import React, { useEffect, useState } from "react";
-// import "./App.css";
+import Pagination from "@mui/material/Pagination";
 import QueryOutput from "../components/QueryOutput";
 
 function Story() {
   const [query] = useState("");
-  // Associated with search bar
   const [result, setresult] = useState([]);
   const [selectState] = useState("news");
-  // console.log(result);
+  const [page, setPage] = React.useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+    fetch(
+      `http://hn.algolia.com/api/v1/search_by_date?query=${
+        query - 1
+      }&hitsPerPage=10&page=${page}`
+    )
+      .then((response) => response.json())
+      .then((r) => {
+        setresult((p) => {
+          return r.hits;
+        });
+      });
+    return (
+      <div className="App">
+        <QueryOutput
+          result={result}
+          query={query}
+          setresult={setresult}
+          stateInfo={selectState}
+        />
+      </div>
+    );
+  };
 
   useEffect(() => {
     fetch(
-      `http://hn.algolia.com/api/v1/search_by_date?query=${query}&hitsPerPage=100`
+      `http://hn.algolia.com/api/v1/search_by_date?query=${query}&hitsPerPage=10&page=${
+        page - 1
+      }`
     )
       .then((response) => response.json())
       .then((r) => {
@@ -21,14 +47,31 @@ function Story() {
       });
   }, []);
   return (
-    <div className="App">
-      {console.log(result)}
+    <div className="App" style={{ backgroundColor: "#F2E7D5" }}>
       <QueryOutput
         result={result}
         query={query}
         setresult={setresult}
         stateInfo={selectState}
       />
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "2%",
+          paddingBottom: "2%",
+          marginTop: "-5px",
+        }}
+      >
+        <Pagination
+          count={10}
+          page={page}
+          color="standard"
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 }
