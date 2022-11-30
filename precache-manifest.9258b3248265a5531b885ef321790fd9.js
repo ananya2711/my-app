ccd5dc95fd166be57043207fe0e37548ba91ed04
ananya2711 <54437908+ananya2711@users.noreply.github.com>
2022-11-30 06:09:23 +0530

    Updates

---
+++ precache-manifest.9258b3248265a5531b885ef321790fd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79bf0c3646a3b92f287b",
+    "url": "/my-app/static/css/main.5e3374a0.chunk.css"
+  },
+  {
+    "revision": "79bf0c3646a3b92f287b",
+    "url": "/my-app/static/js/main.ec1c6c12.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "e5e5659708e4ad40c264",
+    "url": "/my-app/static/js/2.df1029e6.chunk.js"
+  },
+  {
+    "revision": "a92aefd5296fb330341740ae588cbc45",
+    "url": "/my-app/index.html"
+  }
+];
